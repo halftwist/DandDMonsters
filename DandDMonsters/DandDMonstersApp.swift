b58868544e8c334aad6b18c6e187f9c04d422d97
John Kearon <jkearon@hotmail.com>
2025-05-08 08:56:07 -0400

    Initial Commit

---
+++ DandDMonsters/DandDMonstersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DandDMonstersApp.swift
+//  DandDMonsters
+//
+//  Created by John Kearon on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct DandDMonstersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
